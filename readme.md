5fd4e8f6897966f17033018af5a4d0491f79d4db
Marcus Kazmierczak <marcus@mkaz.com>
2021-02-07 08:52:21 -0800

    Init commit

---
+++ readme.md
@@ -0,0 +1,42 @@
+# wprs
+
+A very rough command-line client for WordPress, a toy project I use for scheduling a weekly agenda meeting.
+
+## Setup
+
+To get it to work you need to:
+
+1. Register an app at: https://developer.wordpress.com/
+
+2. Copy the `wprs.conf-example` to `wprs.conf` and fill in app details.
+
+3. Update blog_id, blog_url, and author to match site you want to publish to.
+
+## Usage
+
+### Authentication
+
+Run: `wprs auth`
+
+It will output a URL that you need to open in browser and authenticate, selecting the site you are granting permission.
+
+After authentication, you were be redirected to the site with `?code=XXXX` param, copy the code and enter to get your auth token.
+
+Update your `wprs.conf` with your token.
+
+### Test Connection
+
+Run: `wprs test`
+
+This will use the token and should retrieve the name of the blog.
+
+### Create Post
+
+Run: `wprs create`
+
+First create a file called: `post-template.html` that contains the HTML for the body of the post, then run command and it will create a new post, by default as a draft.
+
+## TODO
+
+At the moment it probably requires some editing of rust source to get it to be usable for anyone else exact purpose. There is still a lot of work to be done to make it more generic. Titles, publish status, dynamic templates, tags, and other bits and bobs.
+
---
+++ src/main.rs
@@ -0,0 +1,177 @@
+use chrono::{DateTime, Duration, Utc};
+use clap::{App, Arg};
+use oauth2;
+use reqwest::blocking::multipart;
+use reqwest::blocking::Client;
+use serde::Deserialize;
+use std::fs;
+use std::io;
+use toml;
+
+#[derive(Clone, Deserialize)]
+struct Config {
+    author: String,
+    auth_url: String,
+    token_url: String,
+    blog_id: u32,
+    blog_url: String,
+    client_id: u32,
+    client_secret: String,
+    token: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct UsersResponse {
+    name: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct PostResponse {
+    #[serde(rename = "short_URL")]
+    short_url: String,
+}
+
+fn main() {
+    let matches = App::new("wprs")
+        .version("0.1.0")
+        .about("A command-line WordPress client")
+        .author("Marcus Kazmierczak")
+        .arg(
+            Arg::new("config")
+                .about("Configuration file")
+                .short('c')
+                .long("config"),
+        )
+        .subcommand(App::new("auth").about("use to login"))
+        .subcommand(App::new("test").about("use to test connection"))
+        .subcommand(App::new("create").about("use to create new post"))
+        .get_matches();
+
+    // read in config
+    let config_file = fs::read_to_string("wprs.conf").unwrap();
+    let config: Config = toml::from_str(&config_file).unwrap();
+
+    match matches.subcommand_name() {
+        Some("auth") => {
+            auth(config.clone());
+        }
+        Some("test") => {
+            connection_test(config.clone());
+        }
+        Some("create") => {
+            create_post(config.clone());
+        }
+        _ => {
+            println!("Command not found");
+        }
+    }
+}
+
+fn create_post(config: Config) {
+    // Create new post
+    let apiurl = format!(
+        "https://public-api.wordpress.com/rest/v1.1/sites/{}/posts/new",
+        config.blog_id
+    );
+
+    // read from template
+    let post_content = fs::read_to_string("post-template.html").unwrap();
+
+    // TODO: improve post template to allow dynamic content
+    // scheduled to run Monday morning
+    // so plus two days is the Wednesday date
+    let dt_wed: DateTime<Utc> = Utc::now() + Duration::days(2);
+    let title = format!("Tinker Weekly: {}", dt_wed.format("%b %e, %Y"));
+    let form = multipart::Form::new()
+        .text("author", config.author)
+        .text("title", title)
+        .text("content", post_content)
+        // TODO: pull from post-template?
+        .text("tags", "team-meeting")
+        // TODO: add flag for create subcommand
+        .text("status", "draft");
+
+    let response = Client::new()
+        .post(&apiurl)
+        .bearer_auth(config.token.clone())
+        .multipart(form)
+        .send();
+
+    match response {
+        Ok(res) => {
+            // Note: res.json() is wrapped in an option
+            match res.json::<PostResponse>() {
+                Ok(post) => {
+                    println!("New post created: {}", post.short_url);
+                }
+                Err(e) => {
+                    println!("Error parsing response: {:?}", e);
+                }
+            }
+        }
+        Err(e) => panic!("Request error: {:?} ", e),
+    };
+}
+
+fn auth(config: Config) {
+    let mut oauth = oauth2::Config::new(
+        format!("{}", config.client_id),
+        config.client_secret,
+        config.auth_url,
+        config.token_url,
+    );
+
+    // Set the desired scopes.
+    oauth = oauth.add_scope("read");
+    oauth = oauth.add_scope("write");
+
+    // Set the URL the user will be redirected to after the authorization process.
+    oauth = oauth.set_redirect_url(config.blog_url);
+
+    // Generate the full authorization URL.
+    // The user needs to go to URL and authenticate.
+    println!("Browse to: {}", oauth.authorize_url());
+
+    // After authentication get ?code=XXXX
+    // Prompt user to enter code to exchange for token
+    println!("After authorization enter code: ");
+    let mut code = String::new();
+    io::stdin()
+        .read_line(&mut code)
+        .expect("error: unable to read user input");
+
+    // remove whitespace
+    let code = code.trim();
+
+    // Use code to get auth token
+    let token_result = oauth.exchange_code(code);
+
+    // TODO: write token to wprs.conf
+    match token_result {
+        Ok(t) => {
+            println!("Update wprs.conf with token");
+            println!("Token: {}", t.access_token);
+        }
+        Err(_) => {
+            println!("Error fetching token");
+        }
+    }
+}
+
+fn connection_test(config: Config) {
+    // test connection
+    let apiurl = format!(
+        "https://public-api.wordpress.com/rest/v1.1/sites/{}",
+        config.blog_id
+    );
+
+    let response = Client::new().get(&apiurl).bearer_auth(config.token).send();
+
+    match response {
+        Ok(res) => {
+            // Note: res.json() is wrapped in a Result
+            println!("Found: {:?}", res.json::<UsersResponse>());
+        }
+        Err(e) => panic!("Request error: {:?} ", e),
+    };
+}
